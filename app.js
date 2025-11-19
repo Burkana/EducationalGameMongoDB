6f6df235923c33446d0687ef70d6c4333d0dea3b
Burkana <burkana@abv.bg>
2025-11-19 20:40:12 +0200

    fix

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // ROUTES
 // ======================
 const authRoutes = require('./routes/authRoutes.js');
-const levelRoutes = require('./routes/levelRoutes.js/index.js');
+const levelRoutes = require('./routes/levelRoutes.js');
 
 app.use('/', authRoutes); // Handles /auth/login, /register, /
 app.use('/', levelRoutes); // Handles /main, /levels/:number
